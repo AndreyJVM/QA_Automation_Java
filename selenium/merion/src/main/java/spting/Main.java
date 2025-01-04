49ce2af8e3b7f71355b04523a4547ecd8f56546e
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2025-01-04 20:33:05 +0300

    Add dependency selenium-java, add simple class, create WebDriverFactory

--- selenium/merion/src/main/java/spting/Main.java
+++ selenium/merion/src/main/java/spting/Main.java
@@ -1,7 +1,21 @@
 package spting;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("New module Merion!");
+
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addExtensions(new File("selenium/merion/src/main/resources/chrome_extensions/User-Agent-Switcher-for-Chrome-Chrome-Web-Store.crx"));
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
     }
 }
---
+++ selenium/merion/src/main/java/spting/factory/WebDriverFactory.java
@@ -0,0 +1,4 @@
+package spting.factory;
+
+public class WebDriverFactory {
+}
