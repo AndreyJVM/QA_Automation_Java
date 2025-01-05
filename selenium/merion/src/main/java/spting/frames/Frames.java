cf376f759bf494de001ee7658f555ad4cfad36c7
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2025-01-05 17:04:13 +0300

    Update Frames.java

--- selenium/merion/src/main/java/spting/frames/Frames.java
+++ selenium/merion/src/main/java/spting/frames/Frames.java
@@ -17,6 +17,13 @@ public class Frames {
         List<WebElement> shouldBeEmpty = driver.findElements(By.cssSelector("body"));
         System.out.println(shouldBeEmpty.get(0).getText());
 
+        driver.switchTo().frame("frame-bottom");
+
+        List<WebElement> shouldHaveBody = driver.findElements(By.cssSelector("body"));
+        System.out.println(shouldHaveBody.get(0).getText());
+
+        driver.switchTo().parentFrame();
+
         driver.quit();
     }
 }
