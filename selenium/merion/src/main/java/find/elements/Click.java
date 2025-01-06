4301235b7554a2eb0aa0d08dfc4c26bbd6b3b473
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2025-01-06 15:02:07 +0300

    Add new class Click.java, SendKeys.java

---
+++ selenium/merion/src/main/java/find/elements/Click.java
@@ -0,0 +1,40 @@
+package find.elements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import spting.factory.WebDriverFactory;
+
+public class Click {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.create("chrome");
+        driver.get("https://habr.com/ru");
+
+        // login button
+        WebElement login = driver.findElement(By.cssSelector(".tm-header-user-menu__login"));
+        login.click();
+
+        driver.quit();
+
+
+        /**
+         * 'click()'
+         * @throws StaleElementReferenceException If the element no longer exists as initially defined
+         * ! - Don`t use variable WebElement
+         */
+
+        WebDriver driverTheInternet = WebDriverFactory.create("chrome");
+        driverTheInternet.get("http://uitestingplayground.com/click");
+
+        // button "Button That Ignores DOM Click Event"
+        WebElement buttonThatIgnoresDOMClickEvent = driverTheInternet.findElement(By.cssSelector("#badButton"));
+        buttonThatIgnoresDOMClickEvent.click();
+
+        driverTheInternet.navigate().refresh();
+        buttonThatIgnoresDOMClickEvent.click();
+
+        driverTheInternet.quit();
+    }
+}
--- selenium/merion/src/main/java/find/elements/WebElementInterface.java
+++ selenium/merion/src/main/java/find/elements/WebElementInterface.java
@@ -1,4 +1,26 @@
 package find.elements;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import spting.factory.WebDriverFactory;
+
+import java.util.List;
+
 public class WebElementInterface {
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverFactory.create("chrome");
+
+        driver.get("https://habr.com/ru");
+        // login button
+        WebElement login = driver.findElement(By.cssSelector(".tm-header-user-menu__login"));
+
+        // output list links class "a"
+        WebElement nav = driver.findElement(By.cssSelector("nav"));
+        List<WebElement> links = nav.findElements(By.cssSelector("a"));
+        System.out.println(links.size()); // 8
+
+        driver.quit();
+    }
 }
