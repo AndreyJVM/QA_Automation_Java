23c57b59b447777b952de5cd8f8a2438b86fb92a
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2025-01-06 12:10:20 +0300

    Add new class WebElementInterface.java

---
+++ selenium/merion/src/main/java/find/elements/WebElementInterface.java
@@ -0,0 +1,4 @@
+package find.elements;
+
+public class WebElementInterface {
+}
--- selenium/merion/src/main/java/spting/frames/Frames.java
+++ selenium/merion/src/main/java/spting/frames/Frames.java
@@ -22,6 +22,7 @@ public class Frames {
         List<WebElement> shouldHaveBody = driver.findElements(By.cssSelector("body"));
         System.out.println(shouldHaveBody.get(0).getText());
 
+
         driver.switchTo().parentFrame();
 
         driver.quit();
