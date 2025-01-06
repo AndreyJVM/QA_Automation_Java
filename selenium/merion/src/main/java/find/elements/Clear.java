acf76febf9dafaa08dd331349947fca91ebdaa96
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2025-01-06 16:52:51 +0300

    Add new class Clear.java

---
+++ selenium/merion/src/main/java/find/elements/Clear.java
@@ -0,0 +1,19 @@
+package find.elements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import spting.factory.WebDriverFactory;
+
+public class Clear {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.create("chrome");
+        driver.get("https://the-internet.herokuapp.com/inputs");
+
+        driver.findElement(By.cssSelector("input")).sendKeys("12345");
+        driver.findElement(By.cssSelector("input")).clear();
+
+        driver.quit();
+    }
+}
--- selenium/merion/src/main/java/find/elements/SendKeys.java
+++ selenium/merion/src/main/java/find/elements/SendKeys.java
@@ -1,4 +1,28 @@
 package find.elements;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import spting.factory.WebDriverFactory;
+
 public class SendKeys {
+
+    public static void main(String[] args) {
+        WebDriver driverTheInternet = WebDriverFactory.create("chrome");
+        driverTheInternet.get("http://uitestingplayground.com/textinput");
+
+        driverTheInternet.findElement(By.cssSelector("#newButtonName"))
+                .sendKeys("Merion");
+
+        driverTheInternet.findElement(By.cssSelector("#newButtonName"))
+                .sendKeys(Keys.BACK_SPACE);
+
+        driverTheInternet.findElement(By.cssSelector("#newButtonName"))
+                .sendKeys(Keys.chord(Keys.SHIFT, Keys.ARROW_UP));
+
+        driverTheInternet.findElement(By.cssSelector("#updatingButton"))
+                .click();
+
+        driverTheInternet.quit();
+    }
 }
