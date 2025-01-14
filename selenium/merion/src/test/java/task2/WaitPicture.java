69dd0e48689af70cbc30938cae53ff0c62739bd1
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2025-01-14 22:01:40 +0300

    Add new class WaitPicture.java

---
+++ selenium/merion/src/test/java/task2/WaitPicture.java
@@ -0,0 +1,20 @@
+package task2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Перейти на сайт https://bonigarcia.dev/selenium-webdriver-java/loading-images.html
+ * Дождаться загрузки 3й картинки
+ * Получить значение атрибута src у 3й картинки
+ * Вывести значение в консоль
+ * */
+
+public class WaitPicture {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+    }
+}
