101e13557c896c1ffdf105b1495983cf4b405fe3
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2025-01-14 21:58:55 +0300

    Add new class RenameButton.java

--- selenium/merion/src/test/java/task2/RenameButton.java
+++ selenium/merion/src/test/java/task2/RenameButton.java
@@ -1,7 +1,12 @@
 package task2;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 /**
  * Перейти на сайт http://uitestingplayground.com/textinput
@@ -14,8 +19,20 @@ public class RenameButton {
     public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.get("http://uitestingplayground.com/textinput");
 
+        driver.findElement(By.cssSelector("#newButtonName"))
+                .sendKeys("Merion");
 
+        driver.findElement(By.cssSelector("#updatingButton"))
+                .click();
+
+        wait.until(ExpectedConditions.textToBe(By.cssSelector("#updatingButton"), "Merion"));
+
+        System.out.println("Button text: " +
+                driver.findElement(By.cssSelector("#updatingButton")).getText());
+
+        driver.quit();
     }
 }
