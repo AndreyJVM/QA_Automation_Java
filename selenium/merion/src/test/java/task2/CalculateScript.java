cb1abb324fd695c5e39a1d5561adcd1ee6025613
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2025-01-15 00:03:48 +0300

    Add new class CalculateScript.java, FormFillingScript.java; Update WaitPicture.java

---
+++ selenium/merion/src/test/java/task2/CalculateScript.java
@@ -0,0 +1,45 @@
+package task2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Открыть страницу https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html
+ * В поле ввода по локатору #delay ввести значение 7 (timeout)
+ * Нажать на кнопки
+        * 7
+        * +(плюс)
+        * 8
+        * =
+ * Дождаться результата. Вывести его в консоль.
+ * */
+
+public class CalculateScript {
+
+    public static void main(String[] args) {
+
+        int timeout = 7;
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html");
+
+        driver.findElement(By.cssSelector("#delay"))
+                .clear();
+        driver.findElement(By.cssSelector("#delay"))
+                .sendKeys(String.valueOf(timeout));
+
+        WebElement keyboard = driver.findElement(By.cssSelector(".keys"));
+
+        keyboard.findElement(By.xpath("//*[text() = '7']")).click();
+        keyboard.findElement(By.xpath("//*[text() = '+']")).click();
+        keyboard.findElement(By.xpath("//*[text() = '8']")).click();
+        keyboard.findElement(By.xpath("//*[text() = '=']")).click();
+
+        // write
+
+        driver.quit();
+    }
+}
--- selenium/merion/src/test/java/task2/WaitPicture.java
+++ selenium/merion/src/test/java/task2/WaitPicture.java
@@ -1,7 +1,14 @@
 package task2;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 /**
  * Перейти на сайт https://bonigarcia.dev/selenium-webdriver-java/loading-images.html
@@ -15,6 +22,17 @@ public class WaitPicture {
     public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html");
 
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("#spinner")));
+
+        List<WebElement> images = driver.findElement(By.cssSelector("#image-container"))
+                        .findElements(By.cssSelector("img"));
+
+        String src = images.get(2).getAttribute("src");
+        System.out.println(src);
+
+        driver.quit();
     }
 }
