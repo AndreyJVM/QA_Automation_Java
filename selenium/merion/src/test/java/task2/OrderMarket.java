754496f972f3505de30119f5dce206ac1cead756
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2025-01-15 20:32:24 +0300

    Add new class OrderMarket.java

---
+++ selenium/merion/src/test/java/task2/OrderMarket.java
@@ -0,0 +1,27 @@
+package task2;
+
+/**
+ * Открыть сайт магазина https://www.saucedemo.com/
+ * Авторизоваться под пользователем standard_user
+ * Добавить в корзину товары:
+ *      Sauce Labs Backpack
+ *      Sauce Labs Bolt T-Shirt
+ *      Sauce Labs Onesie
+ * Перейти в корзину
+ * Нажать Checkout
+ * Заполнить форму данными:
+ *      Имя
+ *      Фамиля
+ *      Почтовый индекс
+ * Нажать continue
+ * Прочитать со стрницы итоговую стоимость ( Total )
+ * Закрыть браузер
+ * Вывести в консоль итоговую стоимость ```
+ * */
+
+public class OrderMarket {
+
+    public static void main(String[] args) {
+
+    }
+}
