3d128806149a05741e01d6ccceb3eea5fba8e770
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2025-01-16 19:06:02 +0300

    Update OrderMarket.java

--- selenium/merion/src/test/java/task2/OrderMarket.java
+++ selenium/merion/src/test/java/task2/OrderMarket.java
@@ -1,5 +1,11 @@
 package task2;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 /**
  * Открыть сайт магазина https://www.saucedemo.com/
  * Авторизоваться под пользователем standard_user
@@ -23,5 +29,22 @@ public class OrderMarket {
 
     public static void main(String[] args) {
 
+        String username = "standard_user";
+        String password = "secret_sauce";
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        driver.findElement(By.cssSelector("#user-name"))
+                        .sendKeys(username);
+        driver.findElement(By.cssSelector("#password"))
+                        .sendKeys(password);
+        driver.findElement(By.cssSelector("#login-button"))
+                        .click();
+
+
+
+        driver.quit();
     }
 }
