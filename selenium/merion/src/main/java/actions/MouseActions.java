1b0b4679f790c92ba8460e9055da76997c43b966
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2025-01-16 22:06:07 +0300

    Update MouseActions.java

--- selenium/merion/src/main/java/actions/MouseActions.java
+++ selenium/merion/src/main/java/actions/MouseActions.java
@@ -1,4 +1,26 @@
 package actions;
 
+import base.selenium.factory.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+
 public class MouseActions {
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverFactory.create("chrome");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        driver.get("https://jspaint.app/#local:241221908e26d");
+
+        WebElement canvas = driver.findElement(By.cssSelector(".main-canvas"));
+
+        new Actions(driver)
+                .clickAndHold(canvas)
+                .moveByOffset(100, -50)
+                .perform();
+        driver.quit();
+    }
 }
