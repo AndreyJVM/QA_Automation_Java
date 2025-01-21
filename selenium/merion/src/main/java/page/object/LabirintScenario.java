e08b4d5bd6c3366dc861f24f2f32b976a21f02a4
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2025-01-21 07:52:27 +0300

    Update: MainPage.java, ResultPage.java, LabirintScenario.java; Add new class CardPage.java

--- selenium/merion/src/main/java/page/object/LabirintScenario.java
+++ selenium/merion/src/main/java/page/object/LabirintScenario.java
@@ -4,55 +4,28 @@ import base.selenium.factory.WebDriverFactory;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import page.object.page.MainPage;
+import page.object.page.ResultPage;
 
 import java.time.Duration;
 import java.util.List;
 
 public class LabirintScenario {
 
-    static WebDriver driver;
-    private static By cartIconLocator = By.cssSelector(".b-header-b-personal-e-icon-count-m-cart");
-
     public static void main(String[] args) {
-        driver = WebDriverFactory.create("chrome");
+        WebDriver driver = WebDriverFactory.create("chrome");
+        MainPage mainPage = new MainPage(driver);
+        ResultPage resultPage = new ResultPage(driver);
 
-        open();
-        searchFor("Java");
-        addAllItemsToCart();
-        checkIconText();
+        mainPage.open();
+        mainPage.searchFor("Java");
+        resultPage.addAllItemsToCart();
+        resultPage.checkIconText();
         openCart();
         checkCardCounter();
         checkPrice();
 
         driver.quit();
-    }
-
-    private static void open() {
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
-        driver.get("https://www.labirint.ru/books/");
-        Cookie cookie = new Cookie("cookie_policy", "1");
-        driver.manage().addCookie(cookie);
-    }
-
-    private static void searchFor(String term) {
-        driver.findElement(By.cssSelector("#search-field")).click();
-        driver.findElement(By.cssSelector("#search-field")).sendKeys("Java", Keys.ENTER);
-    }
-
-    private static void addAllItemsToCart() {
-        int counter = 0;
-        List<WebElement> cards = driver.findElements(By.cssSelector(".product-card"));
-        for (WebElement card : cards) {
-            card.findElement(By.cssSelector(".buy-link")).click();
-            counter++;
-        }
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.textToBe(cartIconLocator, String.valueOf(counter)));
-    }
-
-    private static void checkIconText() {
-        String cardIconCounter = driver.findElement(cartIconLocator).getText();
-        System.out.println("Count products: " + cardIconCounter);
     }
 
     private static void openCart() {
--- selenium/merion/src/main/java/page/object/page/MainPage.java
+++ selenium/merion/src/main/java/page/object/page/MainPage.java
@@ -1,4 +1,29 @@
 package page.object.page;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+
+import java.time.Duration;
+
 public class MainPage {
+
+    private final WebDriver driver;
+
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void open() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        driver.get("https://www.labirint.ru/books/");
+        Cookie cookie = new Cookie("cookie_policy", "1");
+        driver.manage().addCookie(cookie);
+    }
+
+    public void searchFor(String term) {
+        driver.findElement(By.cssSelector("#search-field")).click();
+        driver.findElement(By.cssSelector("#search-field")).sendKeys("Java", Keys.ENTER);
+    }
 }
