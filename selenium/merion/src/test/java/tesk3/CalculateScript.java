41bd4d29a1dc4e900d3a24f3befdb554370b0156
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2025-01-26 21:23:55 +0300

    Update CalculateScript.java

--- selenium/merion/src/test/java/tesk3/CalculateScript.java
+++ selenium/merion/src/test/java/tesk3/CalculateScript.java
@@ -10,27 +10,30 @@ import tesk3.page.CalculatorPage;
  * Открыть страницу https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html
  * В поле ввода по локатору #delay ввести значение 7 (timeout)
  * Нажать на кнопки
-        * 7
-        * +(плюс)
-        * 8
-        * =
+ * 7
+ * +(плюс)
+ * 8
+ * =
  * Дождаться результата. Вывести его в консоль.
- * */
+ */
 
 public class CalculateScript {
 
     public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
-        CalculatorPage calculatorPage = new CalculatorPage(driver).open();
+        CalculatorPage calculatorPage = new CalculatorPage(driver);
 
-        calculatorPage.sendKeysDelay();
+        try {
+            calculatorPage.open();
+            calculatorPage.sendKeysDelay();
 
-        calculatorPage.press("7");
-        calculatorPage.press("+");
-        calculatorPage.press("8");
-        calculatorPage.press("=");
-
-        driver.quit();
+            calculatorPage.press("7");
+            calculatorPage.press("+");
+            calculatorPage.press("8");
+            calculatorPage.press("=");
+        } finally {
+            driver.quit();
+        }
     }
 }
