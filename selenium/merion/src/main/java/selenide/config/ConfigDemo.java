f730cf895a5469d85aa5bba647b339f7f540f09a
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2025-01-29 20:05:17 +0300

    Update ConfigDemo.java

--- selenium/merion/src/main/java/selenide/config/ConfigDemo.java
+++ selenium/merion/src/main/java/selenide/config/ConfigDemo.java
@@ -1,4 +1,30 @@
 package selenide.config;
 
+import com.codeborne.selenide.Configuration;
+
+import static com.codeborne.selenide.Selenide.open;
+
 public class ConfigDemo {
+    public static void main(String[] args) {
+        /// Change settings selenide
+
+        /// Change default timeout 4 sec -> 8 sec
+        /// Default value: 4000 (milliseconds)
+        Configuration.timeout = 8 * 1000; // 1sec <=> 1000ms
+
+        /// Default: false
+        /// true -> ignore UI interface
+        Configuration.headless = true;
+
+        /// Which browser to use. Can be configured either programmatically, via selenide. properties file or by system property "-Dselenide. browser=ie".
+        /// Supported values: "chrome", "firefox", "ie", "edge", "safari".
+        /// Default value: "chrome"
+        Configuration.browser = "firefox";
+
+
+
+
+
+        open("https://habr.com/ru");
+    }
 }
