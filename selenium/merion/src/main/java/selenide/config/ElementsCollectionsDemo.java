905762b90458d6723acc9e3c593e052d653c6ddc
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2025-01-29 20:49:41 +0300

    Update ElementsCollectionsDemo.java

--- selenium/merion/src/main/java/selenide/config/ElementsCollectionsDemo.java
+++ selenium/merion/src/main/java/selenide/config/ElementsCollectionsDemo.java
@@ -1,4 +1,26 @@
 package selenide.config;
 
+import com.codeborne.selenide.*;
+import org.openqa.selenium.Cookie;
+
+import static com.codeborne.selenide.Selenide.*;
+
 public class ElementsCollectionsDemo {
+
+    public static void main(String[] args) {
+        Configuration.baseUrl = "https://www.labirint.ru";
+
+        open("/");
+        Cookie cookie = new Cookie("cookie_policy", "1");
+        WebDriverRunner.getWebDriver().manage().addCookie(cookie);
+
+        open("/");
+        $("#search-field").val("Java").pressEnter();
+
+        ElementsCollection cards = $$(".product-card");
+        cards.forEach(card -> card.find(".buy-link").click());
+
+        $(".b-header-b-personal-e-icon-count-m-cart")
+                .shouldHave(Condition.text(String.valueOf(cards.size())));
+    }
 }
